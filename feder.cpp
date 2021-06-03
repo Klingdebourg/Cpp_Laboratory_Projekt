3e331276332d9cd6825010bd39e3b734e24a65b5
Eric Christfreund <e.christfreund@tum.de>
2021-06-03 13:03:57 +0200

    added constructor for feder class

--- feder.cpp
+++ feder.cpp
@@ -1,6 +1,20 @@
 #include "feder.h"
 
-Feder::Feder()
-{
-
+Feder::Feder(int x, int y) : {
+    int x_, y_;
+    if (x < 0)
+        x_ = 0;
+    else if (x > WINDOW_W)
+        x_ = WINDOW_W;
+    else {
+        x_ = x;
+    }
+    if (y < 0)
+        y_ = 0;
+    else if (y > WINDOW_H)
+        y_ = WINDOW_H;
+    else {
+        y_ = y;
+    }
+    setRect(x_, y_, FEDER_W, FEDER_H);
 }
--- feder.h
+++ feder.h
@@ -3,9 +3,16 @@
 
 #include "QGraphicsRectItem"
 
+#define WINDOW_W 1600
+#define WINDOW_H 1200
+#define FEDER_W 20
+#define FEDER_H 100
+
 class Feder : public QGraphicsRectItem {
 public:
-    Feder();
+    Feder(int x, int y);
+private:
+    int spannung;
 };
 
 #endif // FEDER_H
